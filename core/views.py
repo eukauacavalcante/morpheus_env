7145b0cf5bdee2af6dcef53c289cc87e9d33c5e7
Kauã Silva <kauasilvacavalcante21@gmail.com>
2025-09-19 21:18:05 -0300

    fix(core): adicionar login_required na view principal para restringir acesso a usuários autenticados

--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from http import HTTPStatus
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def main_view(request):
     if request.method == 'GET':
         return render(request, 'index.html')
