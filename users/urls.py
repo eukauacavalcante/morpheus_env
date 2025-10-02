70d6af4ca5c374021f1a0bd9f3551407ef993020
Kauã Silva <kauasilvacavalcante21@gmail.com>
2025-10-02 19:14:45 -0300

    feat(auth): adicionar URLs da página de registro

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import UserRegisterView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('registrar/', UserRegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
