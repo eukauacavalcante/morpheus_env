c7b8f0f39c278d8a3c566528a9bf58994e750d1b
Kauã Silva <kauasilvacavalcante21@gmail.com>
2025-10-02 19:12:59 -0300

    refactor(ai): aprimorar prompt da IA implementada

--- tools/services/ai_analysis.py
+++ tools/services/ai_analysis.py
@@ -6,12 +6,12 @@ from django.conf import settings
 client = Groq(api_key=settings.API_KEY)
 
 
-def analysis_status():
+def get_analysis():
     status = get_system_stats()
     response = client.chat.completions.create(
         model=settings.AI_MODEL,
         messages=[
-            {'role': 'system', 'content': 'Você é um assistente que analisa os dados de uso de CPU, RAM e SSD. Sempre fale diretamente com o usuário, explicando de forma clara e resumida o que cada porcentagem de uso significa. Quando algum dado estiver em nível alto (ex.: CPU acima de 90%, RAM acima de 85% ou SSD acima de 90%), dê um aviso imediato e explique os riscos e o que deve ser feito para prevenir travamentos ou lentidão. Seja objetivo, use apenas um parágrafo e não utilize negrito.'},
+            {'role': 'system', 'content': 'Você é um assistente especializado em analisar dados de desempenho do sistema (CPU, RAM e Memória de armazenamento). Sempre se comunique diretamente com o usuário, de forma clara, simples e em apenas um parágrafo. Não mencione valores exatos de uso de CPU ou RAM, pois variam em tempo real e podem confundir. Explique apenas se o nível está baixo, moderado ou alto, destacando os impactos no desempenho. Caso CPU esteja acima de 90% ou RAM acima de 85%, emita um alerta imediato, explicando riscos como lentidão ou travamentos e sugerindo ações preventivas. Para a Memória de armazenamento (HD/SSD), os valores podem ser citados, já que são estáveis. Evite exageros, seja objetivo e sempre forneça orientações práticas. Não utilize negrito. Retorne tudo em apenas um parágrafo.'},
             {'role': 'user', 'content': f'Atual situação do hardware: {status}'}
         ]
     )
