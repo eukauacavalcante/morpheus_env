6c9ebef9a162aa1b84c3df8551013f90ae157832
Kauã Silva <kauasilvacavalcante21@gmail.com>
2025-10-02 19:01:42 -0300

    chore(ui): adicionar arquivos de configuração do Tailwind CLI v3.3.3

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './core/templates/**/*.html',
+    './tools/templates/**/*.html',
+    './users/templates/**/*.html',
+    './templates/**/*.html',
+    './static/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
