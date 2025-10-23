f3899c1cceb5648ff40547ffc7743ad2e4d8d1e0
Kauã Silva <kauasilvacavalcante21@gmail.com>
2025-10-23 18:19:45 -0300

    style(tools): padroniza a nomeclatura das URLs seguindo boas práticas

--- static/javascript/system-analysis.js
+++ static/javascript/system-analysis.js
@@ -1,5 +1,5 @@
 async function getSystemMetrics() {
-    let request = await fetch('v1/metrics-api')
+    let request = await fetch('api/v1/metrics')
     let response = await request.json()
 
     document.getElementById('cpu').innerHTML = `<p class="text-green-400 font-semibold">${response.data.cpu_percent} %</p>`
@@ -18,7 +18,7 @@ async function aiAnalysis() {
     result_ai.innerHTML = '<p class="text-xl text-yellow-500">A IA está analisando...</p>'
 
     try {
-        const request_ai = await fetch('v1/ai-api')
+        const request_ai = await fetch('api/v1/ai')
         if (!request_ai.ok) {
             throw new Error(`Erro na API: ${request_ai.statusText}`);
         }
--- tools/urls.py
+++ tools/urls.py
@@ -5,8 +5,8 @@ from .views import (AiAPIView, NumberConverterAPIView, NumberConverterView,
 
 urlpatterns = [
     path('monitoramento/', SystemAnalysisView.as_view(), name='system_data'),
-    path('monitoramento/v1/metrics-api', SystemAnalysisAPIView.as_view(), name='system_data_api'),
-    path('monitoramento/v1/ai-api', AiAPIView.as_view(), name='ai_api'),
+    path('monitoramento/api/v1/metrics', SystemAnalysisAPIView.as_view(), name='system_data_api'),
+    path('monitoramento/api/v1/ai', AiAPIView.as_view(), name='ai_api'),
     path('conversor/', NumberConverterView.as_view(), name='num_converter'),
-    path('conversor/v1/api', NumberConverterAPIView.as_view(), name='num_converter_api'),
+    path('conversor/api/v1/converter', NumberConverterAPIView.as_view(), name='num_converter_api'),
 ]
