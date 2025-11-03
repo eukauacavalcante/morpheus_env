4314b3bd62dfbb1e0577db7c0d6dc36df475518c
Kauã Silva <kauasilvacavalcante21@gmail.com>
2025-11-03 20:02:59 -0300

    fix(users): corrige erro nos parâmetros da função

--- users/admin.py
+++ users/admin.py
@@ -15,11 +15,10 @@ class TermsAndPrivacyAdmin(admin.ModelAdmin):
     list_display = ('id', 'updated_at', 'is_active')
     actions = ['send_terms_update_email']
 
-    def send_terms_update_email(self, request):
+    def send_terms_update_email(self, request, queryset):
         users = User.objects.all()
         for user in users:
             send_update_terms_email(user) if settings.EMAIL_MODE else msg_terms_updated(email_sent=False)
         self.message_user(request, 'E-mails enviados com sucesso!')
-        msg_terms_updated()
 
     send_terms_update_email.short_description = 'Enviar e-mail de atualização para todos os usuários'
